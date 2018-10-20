148f10c54ae0bef4ec4f940667bce81465e4b331
akshatapawar129 <akshatapawar129@gmail.com>
2018-10-20 14:21:10 +0530

    new commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -17,10 +17,10 @@
                     <img src="images/smshetty-small.png" class="img img-responsive">
                 </div>
             </li>
-            <li class="active">
+            <li>
                 <a href="index-2.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
-                    <li class="active"><a href="index-2.html">Dashboard v.1</a></li>
+                    <li><a href="index-2.html">Dashboard v.1</a></li>
                     <li><a href="dashboard_2.html">Dashboard v.2</a></li>
                     <li><a href="dashboard_3.html">Dashboard v.3</a></li>
                     <li><a href="dashboard_4_1.html">Dashboard v.4</a></li>
